019db6db149bf6d7820b6886f34d070bd3776457
Andreas Wålm <andreas@walm.net>
2014-06-19 21:21:24 +0200

    Fix init script

--- logstash.sh
+++ logstash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /sbin/setuser app "/opt/logstash/bin/logstash agent -f /conf/logstash.conf --web 2>&1 >> /tmp/logstash.log"
+exec /sbin/setuser app /opt/logstash/bin/logstash agent -f /conf/logstash.conf -web
